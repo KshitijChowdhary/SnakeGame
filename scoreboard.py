ba697e0421f7958a15d649b86fda2a884fa7cf66
Kshitij Chowdhary <138766939+KshitijChowdhary@users.noreply.github.com>
2023-11-23 10:42:02 +0000

    SnakeGame

---
+++ scoreboard.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Arial', 24, 'normal')
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", False, align = ALIGNMENT, font = FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", False, align = ALIGNMENT, font = FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over!", False, align=ALIGNMENT, font=FONT)
+
